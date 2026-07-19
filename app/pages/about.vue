47b6a1595faa0e049a8184e9731d0fd48a132199
Vit Mejzlik <mejvit@gmail.com>
2026-07-19 13:50:24 +0200

    Add logo to about page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -3,6 +3,7 @@
     <h2>O webu</h2>
     <p>Časy pravidelných konání bohoslužeb vychází z údajů uvedených na webu konkrétní farnosti, rádia Proglas, případně TV Noe. Zda a kdy se mše bude konat, ověříte nejlépe přímo na webu farnosti, kde zpravidla naleznete aktuální rozpis bohoslužeb.</p>
     <p>V případě zájmu o <strong>přidání mše svaté</strong> (a velkopátečních obřadů) prosím pište na <a href="mailto:info@mseonline.cz">info@mseonline.cz</a>. <span style="color: #990000">Uvádějte prosím i aktuální časy konání bohoslužeb.</span> Na tento mail pište i v případě, že objevíte nějakou nesrovnalost v časech.</p>
+    <img class="about-page__logo" src="/img/logo.svg" alt="MšeOnline.cz">
   </main>
 </template>
 
@@ -30,5 +31,11 @@
     font-size: 1.05em;
     line-height: 1.9em;
     margin: 0 0 1em 0;
+  }
+
+  .about-page__logo {
+    display: block;
+    width: 10rem;
+    margin-inline: auto;
   }
 </style>
