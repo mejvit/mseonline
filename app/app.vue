c056c53e072980ea106694ca47f95a092853c45d
Vit Mejzlik <mejvit@gmail.com>
2026-07-19 14:23:59 +0200

    Rebuild site logo with HTML and CSS

--- app/app.vue
+++ app/app.vue
@@ -5,7 +5,11 @@
     <Navbar />
 
     <div class="app__page">
-      <h1>Mše Online</h1>
+      <header class="app__header">
+        <h1 class="app__logo">
+          <Logo />
+        </h1>
+      </header>
 
       <NuxtPage />
     </div>
@@ -14,10 +18,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Logo from './components/Logo.vue';
 import Navbar from './components/Navbar.vue';
 
 export default defineComponent({
   components: {
+    Logo,
     Navbar,
   },
 });
@@ -26,12 +32,22 @@ export default defineComponent({
 <style scoped>
   .app__page {
     width: 100%;
+    padding-inline: 1rem;
+  }
+
+  .app__header {
+    padding-block: 4rem 1.5rem;
+  }
+
+  .app__logo {
+    margin: 0;
   }
 
   @media (min-width: 48rem) {
     .app__page {
       width: 90%;
       margin-inline: auto;
+      padding-inline: 0;
     }
   }
 </style>
